b24d3215438fb4dc9b80160e735275e48db9c503
Jacob Rushinski <dsrush2@verizon.net>
2024-10-23 23:53:07 -0400

    Final code commit, reformatted CSS and JS with prettier extension, removed unused code, reviewed code for errors

--- main.js
+++ main.js
@@ -1,66 +1,67 @@
 let correctAnswers = {
-    color: "BLACK",
-    football: "COWBOYS",
-    state: "TEXAS",
-    lounge: "PING-PONG",
-    facial: "YES-FACIAL",
-    glasses: "SOMETIMES-GLASSES",
-    "first-name": "MAINUL",
-    "last-name": "CHOWDHURY",
-    play: "ICE-HOCKEY",
-    time: "AM-FRESHMAN"
+  color: "BLACK",
+  football: "COWBOYS",
+  state: "TEXAS",
+  lounge: "PING-PONG",
+  facial: "YES-FACIAL",
+  glasses: "SOMETIMES-GLASSES",
+  "first-name": "MAINUL",
+  "last-name": "CHOWDHURY",
+  play: "ICE-HOCKEY",
+  time: "AM-FRESHMAN",
 };
 
 let score = 0;
 
 function CheckAnswer() {
-    let currentQuestion = document.querySelector('.current-question');
-    let questionName = currentQuestion.querySelector('input[type="radio"]').name;
-    let userAnswer = getUserInput(questionName);
+  let currentQuestion = document.querySelector(".current-question");
+  let questionName = currentQuestion.querySelector('input[type="radio"]').name;
+  let userAnswer = getUserInput(questionName);
 
-    if (userAnswer) {
-        if (userAnswer === correctAnswers[questionName]) {
-            score++;
-        }
-        goToNextQuestion();
-    } else {
-        alert("Please select an answer!");
+  if (userAnswer) {
+    if (userAnswer === correctAnswers[questionName]) {
+      score++;
     }
+    goToNextQuestion();
+  } else {
+    alert("Please select an answer!");
+  }
 }
 
 function getUserInput(questionName) {
-    let checkedRadio = document.querySelector(`input[name="${questionName}"]:checked`);
-    return checkedRadio ? checkedRadio.value : null;
+  let checkedRadio = document.querySelector(
+    `input[name="${questionName}"]:checked`
+  );
+  return checkedRadio ? checkedRadio.value : null;
 }
 
 function goToNextQuestion() {
-    let currentQuestion = document.querySelector('.current-question');
-    let nextQuestion = currentQuestion.nextElementSibling;
+  let currentQuestion = document.querySelector(".current-question");
+  let nextQuestion = currentQuestion.nextElementSibling;
 
-    if (nextQuestion && nextQuestion.classList.contains('question')) {
-        currentQuestion.style.display = 'none';
-        currentQuestion.classList.remove('current-question');
+  if (nextQuestion && nextQuestion.classList.contains("question")) {
+    currentQuestion.style.display = "none";
+    currentQuestion.classList.remove("current-question");
 
-        nextQuestion.style.display = 'block';
-        nextQuestion.classList.add('current-question');
-    } else {
-        showFinalScreen();
-    }
+    nextQuestion.style.display = "block";
+    nextQuestion.classList.add("current-question");
+  } else {
+    showFinalScreen();
+  }
 }
 
 function showFinalScreen() {
-    let container = document.getElementById('container');
-    container.innerHTML = `
+  let container = document.getElementById("container");
+  container.innerHTML = `
         <h2>Your Score: ${score} / 10</h2>
         <button onclick="restartQuiz()">Restart Quiz</button>
     `;
-    // gives the container the style elements listed below
-    container.style.display = 'flex';
-    container.style.flexDirection = 'column';
-    container.style.justifyContent = 'center';
-    container.style.alignItems = 'center';
+  container.style.display = "flex";
+  container.style.flexDirection = "column";
+  container.style.justifyContent = "center";
+  container.style.alignItems = "center";
 }
 
 function restartQuiz() {
-    window.location.reload(); // Refreshs the page
+  window.location.reload(); // Refreshs the page
 }
