d630123fbc6a23a1b87b291a320970d9945a5290
Jacob Rushinski <dsrush2@verizon.net>
2024-10-23 22:28:43 -0400

    all questions added + changing between questions

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
+function CheckAnswer() {
+    const currentQuestion = document.querySelector('.current-question');
+    const questionName = currentQuestion.querySelector('input[type="radio"]').name;
+    const userAnswer = getUserInput(questionName);
 
-
-function CheckAnswer(){
-    
-    
-    let userAnswer = getUserInput('color');
-    
-
-    if(userAnswer){
-        console.log("question 1 Answer: ", userAnswer);
+    if (userAnswer) {
+        console.log("Answer: ", userAnswer);
         goToNextQuestion();
-    }
-    else{
+    } else {
         alert("Please select an answer!");
     }
-
-    
 }
 
-function getUserInput(questionName){
+function getUserInput(questionName) {
     const checkedRadio = document.querySelector(`input[name="${questionName}"]:checked`);
-    const RadioValue = checkedRadio ? checkedRadio.value : null; // to handle the null
-    let userAnswer = RadioValue;
-    return userAnswer;
+    const RadioValue = checkedRadio ? checkedRadio.value : null;
+    return RadioValue;
 }
-function goToNextQuestion(){
+
+function goToNextQuestion() {
     const currentQuestion = document.querySelector('.current-question');
-    const nextQuestion = document.querySelector('.next-question');
+    const nextQuestion = currentQuestion.nextElementSibling;
 
-    //hide the current question and show the next one
-    currentQuestion.style.display = 'none';
-    currentQuestion.classList.remove('current-question');
+    if (nextQuestion && nextQuestion.classList.contains('question')) {
+        // Hide current question
+        currentQuestion.style.display = 'none';
+        currentQuestion.classList.remove('current-question');
 
-    nextQuestion.style.display = 'block';
-    nextQuestion.classList.add('current-question');
+        // Show next question
+        nextQuestion.style.display = 'block';
+        nextQuestion.classList.add('current-question');
+    } else {
+        alert("Quiz completed!");
+    }
 }
